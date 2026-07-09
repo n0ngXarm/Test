26af602ac371aa8d36eb0035b012862ee3eb17ef
n0ngXarm <pisitpong04@gmail.com>
2026-07-09 10:14:40 +0700

    VeryGood

--- Test.py
+++ Test.py
@@ -1,7 +1,7 @@
-print{"Hello Github"}  
-print{"Hello Github 2"}  
-print{"Hello Github 3"}  
-print{"Hello Github 4"}  
-print{"Hello Github 5"}  
-print{"pisitpong 1231531"}
-print{"pisitpong 494984897498"}
+print("Hello Github")  
+print("Hello Github 2")  
+print("Hello Github 3")  
+print("Hello Github 4")  
+print("Hello Github 5")  
+print("pisitpong 1231531")
+print("pisitpong 494984897498")
